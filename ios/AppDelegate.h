0bb22edf88b28aa7ed393516902a81169cb80b25
liusiqi43 <me@siqi.fr>
2014-12-18 17:52:05 +0100

    Initial Commit

---
+++ ios/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  ios
+//
+//  Created by Siqi Liu on 12/18/14.
+//  Copyright (c) 2014 Alstom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios
+//
+//  Created by Siqi Liu on 12/18/14.
+//  Copyright (c) 2014 Alstom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
